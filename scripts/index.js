4c2ba1a6a0b5155c86fa8de409fa4ccf01834e69
mattb-school <mbrenneman1@ncstudents.niagaracollege.ca>
2025-10-14 22:36:03 +0000

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,112 @@
-// Input elements
-const ckbActive = document.getElementById('chkActiveFilter');
-const ckbClosed = document.getElementById('chkClosedFilter');
-const sltDate = document.getElementById('sltDateFilter');
-const tableBody = document.getElementById('tableBody');
+const collapsableFilts = document.querySelector("#collapsableFilterItems");
+collapsableFilts.style.display = "none";
+let applyFilters = false;
 
-const originRows = Array.from(tableBody.querySelectorAll('tr'));
+const nonCollapsableFilterInputs = document.querySelectorAll(".nonCollapsableFilterInput");
+nonCollapsableFilterInputs[0].addEventListener("input", ShownItemsNumChanged);
+nonCollapsableFilterInputs[1].addEventListener("click", ToggleFilters);
 
-// Functions
-function dateFilter(rows) {
-    const dateFilter = sltDate.value;
-
-    if (dateFilter === 'none')
-    {
-        return rows;
+function ToggleFilters(){
+    if(collapsableFilts.style.display == "none"){
+        nonCollapsableFilterInputs[1].textContent = "Hide Filters"
+        collapsableFilts.style.display = "block";
+        applyFilters = true
+        UpdateList();
     }
+    else{
+        nonCollapsableFilterInputs[1].textContent = "Show Filters"
+        collapsableFilts.style.display = "none";
+        applyFilters = false;
+        UpdateList();
+    }
+}
 
-    const today = new Date();
-    const aWeekBefore = new Date();
-    aWeekBefore.setDate(today.getDate() - 7); // the date of a week ago
-    aWeekBefore.setHours(0, 0, 0, 0); // make time to 0 for date comparison
+const pageButtons = document.querySelectorAll(".pageButtons");
+const pageNumSpan = document.querySelector("#pageNumber");
+let listLength = 0;
 
-    return rows.filter(row => {
-        const rowDate = new Date(row.cells[5].textContent);
-        if (dateFilter === 'new')
-        {
-            return rowDate >= aWeekBefore; // new if it is a week before it was created
-        }
-        else
-        {
-            return rowDate < aWeekBefore; // old if it is a week after it was created
-        }
+
+function ShownItemsNumChanged(){
+    pageNum = 1;
+    pageNumSpan.innerHTML = "Page " + pageNum;
+    pageButtons[0].disabled = true;
+    if(nonCollapsableFilterInputs[0].value == 0 || pageNum * nonCollapsableFilterInputs[0].value >= listLength)
+        pageButtons[1].disabled = true;
+    else
+        pageButtons[1].disabled = false;
+    UpdateList();
+}
+
+pageButtons[0].addEventListener("click", () => {
+    pageNum--;
+    pageNumSpan.innerHTML = "Page " + pageNum;
+    if(pageNum == 1)
+        pageButtons[0].disabled = true;
+    if(pageNum * nonCollapsableFilterInputs[0].value < listLength)
+        pageButtons[1].disabled = false;
+    UpdateList();
+});
+
+pageButtons[1].addEventListener("click", () => {
+    pageNum++;
+    pageNumSpan.innerHTML = "Page " + pageNum;
+    pageButtons[0].disabled = false;
+    if(pageNum * nonCollapsableFilterInputs[0].value >= listLength)
+        pageButtons[1].disabled = true;
+    UpdateList();
+})
+
+const collapsableFilterInputs = document.querySelectorAll(".collapsableFilterInput");
+collapsableFilterInputs.forEach((input) => input.addEventListener("input", () => UpdateList(true)));
+
+function UpdateList(){
+    const NCRs = NCR.GetTabledNCRs(
+        applyFilters, 
+        pageNum, 
+        nonCollapsableFilterInputs[0].value, 
+        collapsableFilterInputs[0].value, 
+        collapsableFilterInputs[1].value, 
+        collapsableFilterInputs[2].value, 
+        collapsableFilterInputs[3].value, 
+        collapsableFilterInputs[4].value, 
+        collapsableFilterInputs[5].value, 
+        collapsableFilterInputs[6].checked, 
+        collapsableFilterInputs[7].checked
+    );
+
+    listLength = NCR.GetFilteredNCRsLength(
+        applyFilters, 
+        collapsableFilterInputs[0].value, 
+        collapsableFilterInputs[1].value, 
+        collapsableFilterInputs[2].value, 
+        collapsableFilterInputs[3].value, 
+        collapsableFilterInputs[4].value, 
+        collapsableFilterInputs[5].value, 
+        collapsableFilterInputs[6].checked, 
+        collapsableFilterInputs[7].checked
+    );
+
+    const tableBody = document.querySelector("#tableBody");
+    tableBody.innerHTML = "";
+
+    NCRs.forEach((ncr) => {
+        const row = tableBody.insertRow();
+        const NCRNoCell = row.insertCell();
+        const PONumCell = row.insertCell();
+        const SupplierCell = row.insertCell();
+        const SAPNoCell = row.insertCell();
+        const DescCell = row.insertCell();
+        const DateOpenedCell = row.insertCell();
+        const StatusCell = row.insertCell();
+
+        NCRNoCell.innerHTML = ncr.ID;
+        PONumCell.innerHTML = ncr.poID;
+        SupplierCell.innerHTML = ncr.supplierName;
+        SAPNoCell.innerHTML = ncr.productID;
+        DescCell.innerHTML = ncr.productDesc;
+        DateOpenedCell.innerHTML = ncr.ncrDateOpened.toLocaleDateString('en-US');
+        StatusCell.innerHTML = ncr.ncrActive ? "Active" : "Closed";
     });
 }
 
-function activeFilter()
-{
-    const showActive = ckbActive.checked;
-    const showClosed = ckbClosed.checked;
-
-    return rows.filter(row => {
-        const status = row.querySelector('.status').textContent;
-        return (showActive && status === 'Active') || (showClosed && status === 'Closed');
-    });
-}
-
-function renderTable(rowsToRender) {
-    tableBody.innerHTML = ''; // clear table
-
-    rowsToRender.forEach(row => {
-        tableBody.appendChild(row);
-    }); // add filted rows
-}
-
-function updateTable() {
-    let filteredRows = filterByStatus(originalRows);
-    
-    filteredRows = filterByDate(filteredRows);
-
-    renderTable(filteredRows);
-}
-
-// Eventlistener
-ckbActive.addEventListener('change', updateTable);
-ckbClosed.addEventListener('change', updateTable);
-sltDate.addEventListener('change', updateTable);
-
-updateTable(); // When the page first loaded, show initial active/closed rows
+ShownItemsNumChanged();
