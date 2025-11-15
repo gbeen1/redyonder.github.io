e15bdbf7cfe774fcf203375ee49a8cf60a427cfc
gbeen1 <gbeen1@ncstudents.niagaracollege.ca>
2025-11-14 22:47:27 -0500

    add page navigation function
    HTML : add page number input field
    CSS : minor change for pagebuttons, and change pagenumber for alignment
    JS : add pagenavigation function, minor change on updatelist function, change Initial function FilterChanged to UpdateList

--- scripts/index.js
+++ scripts/index.js
@@ -31,41 +31,73 @@ function ToggleFilters(){
 
 
 const pageButtons = document.querySelectorAll(".pageButtons");
-const pageNumSpan = document.querySelector("#pageNumber");
+//const pageNumSpan = document.querySelector("#pageNumber"); // GeonUk : OLD code, no more use page number span
+const pageInput = document.querySelector("#pageInput");      // GeonUk : using instead of pageNumSpan
 let listLength = 0;
+let pageNum = 1;                                             // GeonUk : initial page number is 1
 
 // sets page to 1, updates list, enables or disables next and back buttons as required
-function FilterChanged(){
+//function FilterChanged(){
+//    pageNum = 1;
+//    pageNumSpan.innerHTML = "Page " + pageNum;
+//    pageButtons[0].disabled = true;
+//    UpdateList();
+//    if(nonCollapsableFilterInputs[0].value == 0 || pageNum * nonCollapsableFilterInputs[0].value >= listLength)
+//        pageButtons[1].disabled = true;
+//    else
+//        pageButtons[1].disabled = false;
+//}                                                          //GeonUk : OLD code, replaced by below code
+function FilterChanged() {
     pageNum = 1;
-    pageNumSpan.innerHTML = "Page " + pageNum;
-    pageButtons[0].disabled = true;
     UpdateList();
-    if(nonCollapsableFilterInputs[0].value == 0 || pageNum * nonCollapsableFilterInputs[0].value >= listLength)
-        pageButtons[1].disabled = true;
-    else
-        pageButtons[1].disabled = false;
 }
 
 // back page button updates list to new page, enables or disables page buttons as required
+//pageButtons[0].addEventListener("click", () => {
+//    pageNum--;
+//    pageNumSpan.innerHTML = "Page " + pageNum;
+//    UpdateList();
+//    if(pageNum == 1)
+//        pageButtons[0].disabled = true;
+//    if(pageNum * nonCollapsableFilterInputs[0].value < listLength)
+//        pageButtons[1].disabled = false;
+//    document.querySelector("thead").scrollIntoView();
+//});                                                        //GeonUk : OLD code, replaced by below code
 pageButtons[0].addEventListener("click", () => {
     pageNum--;
-    pageNumSpan.innerHTML = "Page " + pageNum;
     UpdateList();
-    if(pageNum == 1)
-        pageButtons[0].disabled = true;
-    if(pageNum * nonCollapsableFilterInputs[0].value < listLength)
-        pageButtons[1].disabled = false;
     document.querySelector("thead").scrollIntoView();
 });
 
 // next page button updates list to new page, enables or disables page buttons as required
+//pageButtons[1].addEventListener("click", () => {
+//    pageNum++;
+//    pageNumSpan.innerHTML = "Page " + pageNum;
+//    UpdateList();
+//    pageButtons[0].disabled = false;
+//    if(pageNum * nonCollapsableFilterInputs[0].value >= listLength)
+//        pageButtons[1].disabled = true;
+//    document.querySelector("thead").scrollIntoView();
+//});                                                        //GeonUk : OLD code, replaced by below code
 pageButtons[1].addEventListener("click", () => {
     pageNum++;
-    pageNumSpan.innerHTML = "Page " + pageNum;
     UpdateList();
-    pageButtons[0].disabled = false;
-    if(pageNum * nonCollapsableFilterInputs[0].value >= listLength)
-        pageButtons[1].disabled = true;
+    document.querySelector("thead").scrollIntoView();
+});
+
+// GeonUk : update the list when user enter a number in the input window and press enter
+pageInput.addEventListener("change", () => {
+    const sltNum = parseInt(nonCollapsableFilterInputs[0].value, 10);
+    const totalPage = (sltNum === 0) ? 1 : Math.ceil(listLength / sltNum);
+    let inputPage = parseInt(pageInput.value, 10);
+    if (inputPage < 1) {
+        inputPage = 1;
+    }
+    else if (inputPage > totalPage) {
+        inputPage = totalPage;
+    }
+    pageNum = inputPage;
+    UpdateList();
     document.querySelector("thead").scrollIntoView();
 });
 
@@ -99,6 +131,27 @@ function UpdateList(){
         collapsableFilterInputs[6].value
     );
 
+    // GeonUk : add page calculation codes
+    const sltNum = parseInt(nonCollapsableFilterInputs[0].value, 10);
+    let totalPage = 1;
+    //GeonUk : page calculation only when sltNum > 0
+    if (sltNum > 0) {
+        totalPage = Math.ceil(listLength / sltNum);
+    }
+    //GeonUk : prevent totalPage being 0 when no record found
+    if (totalPage === 0) {
+        totalPage = 1;
+        pageNum = 1;
+    } 
+    //GeonUk : if pageNum is bigger than totalPage, set pageNum to totalPage
+    if (pageNum > totalPage) {
+        pageNum = totalPage;
+    }
+    pageInput.value = pageNum;
+    pageInput.max = totalPage;
+    pageButtons[0].disabled = (pageNum <= 1);
+    pageButtons[1].disabled = (pageNum >= totalPage);
+
     const tableBody = document.querySelector("#tableBody");
     tableBody.innerHTML = "";
 
@@ -131,7 +184,8 @@ function UpdateList(){
 }
 
 // initial list on startup
-FilterChanged();
+//FilterChanged();      //GeonUk : OLD code, replaced by below code
+UpdateList();
 
 document.querySelector("#btnHelp").addEventListener("click", function(event){
     event.preventDefault();
